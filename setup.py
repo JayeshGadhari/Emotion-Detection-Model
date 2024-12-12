d0e5feb1fa95955692d8859613466fb8d8df47a5
Jayesh <jgadhari5@gmail.com>
2024-12-12 14:45:02 +0530

    add cookie cutter template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='A ml model used to detect emotion from text',
+    author='jayesh',
+    license='',
+)
